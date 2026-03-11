3578068016de3f939e92a405d8b95b2ba99bee07
lukasz <lukasz.darmofal@cyfroweszkoly.pl>
2026-03-11 09:07:41 +0100

    commit3

--- src/routes/taskroutes.ts
+++ src/routes/taskroutes.ts
@@ -27,4 +27,38 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.put('/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const updatedTask: Task | null = await TaskModel.findByIdAndUpdate(id, req.body, {new: true})
+
+        if (!updatedTask) {
+            res.status(404).json({error: 'Task not found'});
+            return;
+        }
+        res.status(200).json({message: 'Task updated successfully.', task: updatedTask});
+
+    } catch (e) {
+        res.status(500).json({error: e});
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+
+        const {id} = req.params;
+        const deletedTask: Task | null = await TaskModel.findByIdAndDelete(id)
+        if (!deletedTask) {
+            res.status(404).json({error: 'Task not found'});
+            return;
+        }
+        res.json({message: 'Task deleted successfully.', task: deletedTask});
+
+    } catch (e) {
+        const err = e instanceof Error ? e.message : "Unknown error";
+        res.status(500).json({error: err});
+    }
+})
+
+
 export default router;
